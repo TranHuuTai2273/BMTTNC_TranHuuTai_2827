0ee5e941af556049300e9ccc5fd98f4736054bfd
TranHuuTai2273 <tranhuutai22072003@gmail.com>
2025-06-17 16:17:36 +0700

    Add files via upload

---
+++ lab04/dh_key_pair/server.py
@@ -0,0 +1,6 @@
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives import serialization
+
+def generate_dh_parameters():
+    parameters = dh.generate_parameters(generator=2, key_size=2048)
+    return parameters
